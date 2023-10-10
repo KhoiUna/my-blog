1789c60a8c5e0a22d4d38761eb6f16cf3f7d101a
KhoiUna <thorwaitson@gmail.com>
2023-10-10 12:40:33 -0500

    modified:   create.sh

--- create.sh
+++ create.sh
@@ -6,7 +6,7 @@ YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 
 mkdir -p "src/posts/$YEAR/$MONTH"
-FILENAME="src/posts/$YEAR/$MONTH/$TITLEF.md"
+FILENAME="src/posts/$YEAR/$MONTH/${TITLEF,,}.md"
 
 cat <<EOF > $FILENAME
 ---
